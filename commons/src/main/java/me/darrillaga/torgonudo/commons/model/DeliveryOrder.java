49c226c485b140ea5ba0ef24cf242535fdb883b9
Damián Arrillaga <damian.arrillaga@gmail.com>
2016-07-18 19:36:33 -0300

    Receive basic delivery order

--- commons/src/main/java/me/darrillaga/torgonudo/commons/model/DeliveryOrder.java
+++ commons/src/main/java/me/darrillaga/torgonudo/commons/model/DeliveryOrder.java
@@ -1,6 +1,11 @@
 package me.darrillaga.torgonudo.commons.model;
 
+import com.google.firebase.database.Exclude;
+import com.google.firebase.database.PropertyName;
+
 import java.util.Date;
+
+import java8.util.stream.RefStreams;
 
 public class DeliveryOrder {
 
@@ -15,6 +20,16 @@ public class DeliveryOrder {
 
         public String value() {
             return value;
+        }
+
+        public static Status findValue(String value) {
+            if (value == null) {
+                return null;
+            }
+
+            return RefStreams.of(Status.values())
+                    .filter(value::equals)
+                    .findFirst().orElse(null);
         }
     }
 
@@ -49,12 +64,24 @@ public class DeliveryOrder {
         this.price = price;
     }
 
+    @Exclude
     public Status getStatus() {
         return status;
     }
 
+    @Exclude
     public void setStatus(Status status) {
         this.status = status;
+    }
+
+    @PropertyName("status")
+    public String getStatusString() {
+        return status != null ? status.value() : null;
+    }
+
+    @PropertyName("status")
+    public void setStatusString(String status) {
+        setStatus(status != null ? Status.findValue(status) : null);
     }
 
     public Date getCreatedAt() {
--- viewer/src/main/java/me/darrillaga/torgonudo/viewer/viewpendingorders/view/PendingDeliveryOrdersActivity.java
+++ viewer/src/main/java/me/darrillaga/torgonudo/viewer/viewpendingorders/view/PendingDeliveryOrdersActivity.java
@@ -1,8 +1,29 @@
 package me.darrillaga.torgonudo.viewer.viewpendingorders.view;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import me.darrillaga.torgonudo.commons.BaseActivity;
+import me.darrillaga.torgonudo.viewer.R;
+import se.emilsjolander.intentbuilder.IntentBuilder;
 
 
-public class PendingDeliveryOrdersActivity extends AppCompatActivity {
+@IntentBuilder
+public class PendingDeliveryOrdersActivity extends BaseActivity {
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        PendingDeliveryOrdersActivityIntentBuilder.inject(getIntent(), this);
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_base);
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(
+                        R.id.container,
+                        new PendingDeliveryOrdersFragmentBuilder().build(),
+                        PendingDeliveryOrdersFragment.class.getName()
+                ).commit();
+    }
 }
